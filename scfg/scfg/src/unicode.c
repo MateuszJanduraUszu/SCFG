78249d873085e105d117ff9df2b539f428d16d8c
MateuszJanduraUszu <Mati4321@interia.pl>
2022-10-21 23:54:01 +0200

    Improve conversion between Unicode and UTF-8

--- scfg/scfg/src/unicode.c
+++ scfg/scfg/src/unicode.c
@@ -7,48 +7,156 @@
 #include <scfgpch.h>
 #include <stdlib.h>
 
-// FUNCTION scfg_unicode_to_buffer
-scfg_error_t _SCFG_CONV scfg_unicode_to_buffer(
-    const wchar_t* const _Unc, const size_t _Size, scfg_buffer_t* const _Buf) {
+scfg_error_t _SCFG_CONV scfg_unicode_to_utf8_required_buffer_size(
+    const wchar_t* _Data, size_t _Data_size, size_t* const _Buf_size) {
+    if (!_Buf_size) { // do not store any informations
+        return scfg_error_success;
+    }
+
+    *_Buf_size = 0;
+    while (_Data_size-- > 0) {
+        if (*_Data <= 0x7F) { // 1 byte per word
+            ++*_Buf_size;
+        } else if (*_Data <= 0x07FF) { // 2 bytes per word
+            *_Buf_size += 2;
+        } else if (*_Data <= 0xFFFF) { // 3 bytes per word
+            *_Buf_size += 3;
+        } else if (*_Data <= 0x0010'FFFF) { // 4 bytes per word
+            *_Buf_size += 4;
+        } else { // word too large, see RFC3629
+            return scfg_error_code_point_too_large;
+        }
+
+        ++_Data;
+    }
+
+    return scfg_error_success;
+}
+
+scfg_error_t _SCFG_CONV scfg_utf8_to_unicode_required_buffer_size(
+    const char* _Data, size_t _Data_size, size_t* const _Buf_size) {
+    if (!_Buf_size) { // do not store any informations
+        return scfg_error_success;
+    }
+
+    *_Buf_size = 0;
+    while (_Data_size > 0) {
+        if ((*_Data & 0x80) == 0) { // 1 byte per word
+            --_Data_size;
+            ++_Data;
+        } else if ((*_Data & 0xE0) == 0xC0) { // 2 bytes per word
+            _Data_size -= 2;
+            _Data      += 2;
+        } else if ((*_Data & 0xF0) == 0xE0) { // 3 bytes per word
+            _Data_size -= 3;
+            _Data      += 3;
+        } else if ((*_Data & 0xF8) == 0xF0) { // 4 bytes per word
+            _Data_size -= 4;
+            _Data      += 4;
+        } else { // word too large, see RFC3629
+            return scfg_error_code_point_too_large;
+        }
+
+        ++*_Buf_size;
+    }
+
+    return scfg_error_success;
+}
+
+scfg_error_t _SCFG_CONV scfg_unicode_to_utf8(
+    const wchar_t* _Data, size_t _Size, scfg_utf8_buffer_t* const _Buf) {
     if (!_Buf || _Buf->_Size == 0) {
         return scfg_error_invalid_buffer;
     }
 
-    if (_Size == 0) {
-        return scfg_error_success;
-    }
-
-    uint8_t* _Ptr;
-    const scfg_error_t _Err = scfg_get_associated_buffer(_Buf, &_Ptr);
+    size_t _Buf_size  = 0;
+    scfg_error_t _Err = scfg_unicode_to_utf8_required_buffer_size(_Data, _Size, &_Buf_size);
     if (_Err != scfg_error_success) {
         return _Err;
     }
 
-#pragma warning(suppress : 4996) // C4996: Use wcstombs_s() instead
-    if (wcstombs((char*) _Ptr, _Unc, _Size) == (size_t) -1) {
-        return scfg_error_invalid_code_point;
-    } else {
-        return scfg_error_success;
+    if (_Buf->_Size < _Buf_size) {
+        return scfg_error_buffer_too_small;
     }
+
+    char* _Ptr;
+    _Err = scfg_get_associated_buffer(_Buf, scfg_buffer_type_utf8, &_Ptr);
+    if (_Err != scfg_error_success) {
+        return _Err;
+    }
+
+    while (_Size-- > 0) {
+        if (*_Data <= 0x7F) { // 1 byte per word
+            *(_Ptr++) = (char)*_Data; // no leading byte
+        } else if (*_Data <= 0x07FF) { // 2 bytes per word
+            *(_Ptr++) = (char) (((*_Data >> 6) & 0x1F) | 0xC0); // leading byte
+            *(_Ptr++) = (char) ((*_Data & 0x3F) | 0x80);
+        } else if (*_Data <= 0xFFFF) { // 3 bytes per word
+            *(_Ptr++) = (char) (((*_Data >> 12) & 0x0F) | 0xE0); // leading byte
+            *(_Ptr++) = (char) (((*_Data >> 6) & 0x3F) | 0x80);
+            *(_Ptr++) = (char) ((*_Data & 0x3F) | 0x80);
+        } else if (*_Data <= 0x0010'FFFF) { // 4 bytes per word
+            *(_Ptr++) = (char) (((*_Data >> 18) & 0x07) | 0xF0); // leading byte
+            *(_Ptr++) = (char) (((*_Data >> 12) & 0x3F) | 0x80);
+            *(_Ptr++) = (char) (((*_Data >> 6) & 0x3F) | 0x80);
+            *(_Ptr++) = (char) ((*_Data & 0x3F) | 0x80);
+        } else { // word too large, see RFC3629
+            return scfg_error_code_point_too_large;
+        }
+
+        ++_Data;
+    }
+
+    return scfg_error_success;
 }
 
-// FUNCTION scfg_unicode_from_buffer
-scfg_error_t _SCFG_CONV scfg_unicode_from_buffer(
-    wchar_t* const _Unc, const size_t _Size, scfg_buffer_t* const _Buf) {
-    if (!_Buf || !_Unc || _Size == 0) {
+scfg_error_t _SCFG_CONV scfg_utf8_to_unicode(
+    const char* _Data, size_t _Size, scfg_unicode_buffer_t* const _Buf) {
+    if (!_Buf || _Buf->_Size == 0) {
         return scfg_error_invalid_buffer;
     }
-    
-    uint8_t* _Ptr;
-    const scfg_error_t _Err = scfg_get_associated_buffer(_Buf, &_Ptr);
+
+    size_t _Buf_size  = 0;
+    scfg_error_t _Err = scfg_utf8_to_unicode_required_buffer_size(_Data, _Size, &_Buf_size);
     if (_Err != scfg_error_success) {
         return _Err;
     }
 
-#pragma warning(suppress : 4996) // C4996: Use mbstowcs_s() instead
-    if (mbstowcs(_Unc, _Ptr, _Buf->_Size) == (size_t) -1) {
-        return scfg_error_invalid_code_point;
-    } else {
-        return scfg_error_success;
+    if (_Buf->_Size < _Buf_size) {
+        return scfg_error_buffer_too_small;
     }
+
+    wchar_t* _Ptr;
+    _Err = scfg_get_associated_buffer(_Buf, scfg_buffer_type_unicode, &_Ptr);
+    if (_Err != scfg_error_success) {
+        return _Err;
+    }
+
+    while (_Size > 0) {
+        if ((*_Data & 0x80) == 0) { // 1 byte per word
+            *_Ptr = (wchar_t) *(_Data++);
+            --_Size;
+        } else if ((*_Data & 0xE0) == 0xC0) { // 2 bytes per word
+            *_Ptr  = (wchar_t) ((*(_Data++) & 0x1F) << 6);
+            *_Ptr |= (wchar_t) (*(_Data++) & 0x3F);
+            _Size -= 2;
+        } else if ((*_Data & 0xF0) == 0xE0) { // 3 bytes per word
+            *_Ptr  = (wchar_t) ((*(_Data++) & 0x0F) << 12);
+            *_Ptr |= (wchar_t) ((*(_Data++) & 0x3F) << 6);
+            *_Ptr |= (wchar_t) (*(_Data++) & 0x3F);
+            _Size -= 3;
+        } else if ((*_Data & 0xF8) == 0xF0) { // 4 bytes per word
+            *_Ptr  = (wchar_t) ((*(_Data++) & 0x07) << 18);
+            *_Ptr |= (wchar_t) ((*(_Data++) & 0x3F) << 12);
+            *_Ptr |= (wchar_t) ((*(_Data++) & 0x3F) << 6);
+            *_Ptr |= (wchar_t) (*(_Data++) & 0x3F);
+            _Size -= 4;
+        } else { // word too large, see RFC3629
+            return scfg_error_code_point_too_large;
+        }
+
+        ++_Ptr;
+    }
+
+    return scfg_error_success;
 }
